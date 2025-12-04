31999944092216a4f88e0280574411c17ab2fd37
Terren Handy <213094058+thandy1@users.noreply.github.com>
2025-12-04 17:29:27 -0600

    Track and add new problem sets

---
+++ chapter-8/try_it_yourself/8-11_archived_messages.py
@@ -0,0 +1,17 @@
+messages = ["Hello", "Greetings", "Hi", "Good Night", "Good Morning",]
+sent_messages = []
+
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+def send_messages(messages):
+    while messages:
+       current_msg = messages.pop(0)
+       print(current_msg)
+       sent_messages.append(current_msg) 
+
+send_messages(messages[:])
+
+print(f"Original messages: {messages}")
+print(f"Sent Messages: {sent_messages}")
---
+++ chapter-8/try_it_yourself/8-9_messages.py
@@ -0,0 +1,7 @@
+messages = ["Hello", "Greetings", "Hi", "Good Night", "Good Morning",]
+
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+show_messages(messages)
